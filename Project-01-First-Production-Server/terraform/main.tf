51b138c1fda4b40f4f3befe993c8c7edc253b699
tintumaria09 <tintu.maria.98.matt@gmail.com>
2026-08-07 22:33:49 +0530

    Update security group for web server deployment

--- Project-01-First-Production-Server/terraform/main.tf
+++ Project-01-First-Production-Server/terraform/main.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "web_server"{
 
 resource "aws_security_group" "ssh_access"{
   name = "lunaria-ssh-sg"
-  description = "Allow SSH access from my IP"
+  description = "Allow SSH access from my IP and HTTP web traffic"
 
   ingress{
     description ="SSH"
@@ -27,6 +27,14 @@ resource "aws_security_group" "ssh_access"{
     to_port = 22
     protocol = "tcp"
     cidr_blocks = [var.my_ip]
+  }
+
+  ingress{
+    description ="HTTP"
+    from_port = 80
+    to_port =80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress{
